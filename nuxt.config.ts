7925084ab4b1b17720c2809548a72317f7e830f3
Max Steinwand <msteinwand@kues.de>
2024-07-26 12:28:01 +0200

    entfernen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,9 +16,6 @@ export default defineNuxtConfig({
     "@nuxtjs/seo"
   ],
 
-  ogImage: {
-    enabled: false,
-  },
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
     "components:extend": (components) => {
